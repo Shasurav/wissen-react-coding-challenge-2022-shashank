84c543645ed0c3a7ac3c85809e9dbb1cff427b1a
Shasurav <shasurav@gmail.com>
2022-03-28 12:31:14 +0530

    1 file modified

--- app.tsx
+++ app.tsx
@@ -1,12 +1,18 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import './style.css';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import Login from './components/login';
 import View from './components/view';
 
 const Start = () => {
+  const dispatch = useDispatch();
   const { isLoggedIn } = useSelector((state) => state.userReducer);
+
+  useEffect(() => {
+    if (localStorage.getItem('token'))
+      dispatch({ type: 'SET_LOGGEDIN', payload: true });
+  });
   return (
     <Fragment>
       {!isLoggedIn && <Login />}
